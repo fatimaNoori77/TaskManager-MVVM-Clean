cde0598a35373b521997b1e40b6a1c67123fef88
fatemeh <fatimanoori1998@gmail.com>
2025-10-02 13:53:26 +0330

    remove MyApplication.kt body

--- app/src/main/java/ir/noori/taskmanager/MyApplication.kt
+++ app/src/main/java/ir/noori/taskmanager/MyApplication.kt
@@ -5,9 +5,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class MyApplication : Application()
